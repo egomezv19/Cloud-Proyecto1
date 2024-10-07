30e0f0ef9836ceec835409a000eb01e784344c04
nataliawg <natalia.watson@utec.edu.pe>
2024-10-06 22:52:10 -0500

    docker compose aaa

--- microservice_empresa_empleo/database.py
+++ microservice_empresa_empleo/database.py
@@ -1,16 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:nosejajaja123-@localhost/microservicio2_db"
+load_dotenv(dotenv_path='.env/db.env')
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-try:
-    db = SessionLocal()
-    print("Conexión a la base de datos exitosa :)")
-except Exception as e:
-    print(f"Error al conectar con la base de datos: {e}")
-finally:
-    db.close()
+
