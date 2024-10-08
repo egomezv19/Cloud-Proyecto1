490ce339ae448f59db67556d9167355fe9f42c52
Natalia Watson <165820132+nataliawg@users.noreply.github.com>
2024-10-08 00:29:11 -0500

    Delete service_new directory

--- service_new/src/main/java/com/example/service_new/Inscripcion/InscripcionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.service_new.Inscripcion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
-}
--- service_new/src/main/java/com/example/service_new/Programa/ProgramaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.service_new.Programa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProgramaRepository extends JpaRepository<Programa, Long> {
-}
